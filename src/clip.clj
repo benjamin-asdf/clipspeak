b877d867a4766639bcd5d17b7022ff8b652f8b77
Benni Schwerdtner <Benjamin.Schwerdtner@gmail.com>
2025-07-03 09:29:55 +0200

    Curr

--- src/clip.clj
+++ src/clip.clj
@@ -32,7 +32,7 @@
   [{:keys [file speed] :as snip}
    out-file]
   (assert
-   (and speed (<= 0.5 speed 2.0)))
+   (and speed (<= 0.5 speed 3.0)))
   (let [speed-cmd (format
                    "ffmpeg -y -i %s -filter:v \"setpts=PTS/%s\" -filter:a \"atempo=%s\" %s"
                    file
@@ -98,12 +98,14 @@
     (process/shell "ffmpeg" "-f" "concat" "-safe" "0" "-y" "-i" "concat-them" "-c" "copy" (str out-file)))
   (->snip out-file nil nil))
 
+(def ^:dynamic *dir*)
+
 (defn
   doit-1!
   ([snip]
-   (doit-1!
-    snip
-    (str (random-uuid) ".mp4")))
+   (doit-1! snip (fs/file
+                  *dir*
+                  (str (random-uuid) ".mp4"))))
   ([{:keys [concated-snips]
      :as snip}
     out-file]
@@ -136,241 +138,360 @@
 (defn doit! [snip out-file]
   (let [file (fs/absolutize (fs/file out-file))]
     (fs/with-temp-dir [dir {}]
-      (fs/copy (:file (doit-1! snip)) file {:replace-existing true}))))
+      (binding [*dir* dir]
+        (fs/copy (:file (doit-1! snip)) file {:replace-existing true})))))
+
+(comment
+
+  (->
+   (concat-snips
+    (->snip "/home/benj/video/nevero/nevero.mp4" nil nil)
+    (->snip "/home/benj/video/nevero/output.mp4" nil nil))
+   (doit! "/tmp/trs-3.mp4"))
+
+  ;; #object[sun.nio.fs.UnixPath 0x3fce499e "/tmp/trs-3.mp4"]
+
+
+  (->snip
+   "/home/benj/video/w/2025-01-30_19-06-09.mkv"
+   )
+
+  (->
+   (concat-snips
+    (->snip "/home/benj/video/counting/2025-02-05_18-45-32.mkv" nil "00:00:59")
+    (->snip "/home/benj/video/counting/2025-02-05_18-50-49.mkv" "00:00:10" "00:47:26")
+    (->snip "/home/benj/video/counting/2025-02-05_18-50-49.mkv" "00:49:27" nil))
+   (doit! "/tmp/trs-3.mp4"))
+
+  ;; #object[sun.nio.fs.UnixPath 0xb8500e3 "/tmp/trs-3.mp4"]
+
+
+
+
+
+  ;; -----------------
+  ;; numbers
+  ;;
+
+  (->
+   (concat-snips
+    (->snip "/home/benj/video/2025-02-08_08-44-06.mkv" nil "00:21:00")
+    (->snip "/home/benj/video/2025-02-08_08-44-06.mkv" "00:22:34" "00:30:29")
+    (->snip "/home/benj/video/2025-02-08_08-44-06.mkv" "00:30:51" "00:37:26")
+    (->snip "/home/benj/video/2025-02-08_08-44-06.mkv" "00:38:22" nil)
+    ;; --------------------
+    ;; (->snip "/home/benj/video/2025-02-08_15-14-08.mkv" nil nil)
+    ;; (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" nil "00:28:54")
+    ;; (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "00:30:00" "00:30:29")
+    ;; (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "00:31:46" "00:32:00")
+    ;; (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "00:32:56" "00:34:05")
+    ;; (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "00:35:37" "00:38:59")
+    ;; (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "00:40:08" "00:43:00")
+    ;; (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "00:46:22" "01:19:56")
+    ;; (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "01:25:20" "01:26:13")
+    ;; (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "01:32:42" "01:36:26")
+    ;; (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "01:44:56" nil)
+    )
+   (doit! "/tmp/trs-4.mp4"))
+
+
+
+  (->
+   (concat-snips
+    ;; (->snip "/home/benj/video/2025-02-08_08-44-06.mkv" nil "00:21:00")
+    ;; (->snip "/home/benj/video/2025-02-08_08-44-06.mkv" "00:22:34" "00:30:29")
+    ;; (->snip "/home/benj/video/2025-02-08_08-44-06.mkv" "00:30:51" "00:37:26")
+    ;; (->snip "/home/benj/video/2025-02-08_08-44-06.mkv" "00:38:22" nil)
+    ;; --------------------
+    ;; (->snip "/home/benj/video/2025-02-08_15-14-08.mkv" nil nil)
+
+    (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" nil "00:28:54")
+    (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "00:30:00" "00:30:29")
+    (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "00:31:46" "00:32:00")
+    (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "00:32:56" "00:34:05")
+    (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "00:35:37" "00:38:59")
+    (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "00:40:08" "00:43:00")
+    (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "00:46:22" "01:19:56")
+    (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "01:25:20" "01:26:13")
+    (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "01:32:42" "01:36:26")
+    (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "01:44:56" nil))
+   (doit! "/tmp/trs-4-3.mp4"))
+
+
+
+  (->
+   (concat-snips
+    ;; (->snip "/home/benj/video/2025-02-08_08-44-06.mkv" nil "00:21:00")
+    ;; (->snip "/home/benj/video/2025-02-08_08-44-06.mkv" "00:22:34" "00:30:29")
+    ;; (->snip "/home/benj/video/2025-02-08_08-44-06.mkv" "00:30:51" "00:37:26")
+    ;; (->snip "/home/benj/video/2025-02-08_08-44-06.mkv" "00:38:22" nil)
+    ;; --------------------
+    (->snip "/home/benj/video/2025-02-08_15-14-08.mkv" nil nil)
+
+    (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" nil "00:28:54")
+    (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "00:30:00" "00:30:29")
+    (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "00:31:46" "00:32:00")
+    (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "00:32:56" "00:34:05")
+    (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "00:35:37" "00:38:59")
+    (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "00:40:08" "00:43:00")
+    (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "00:46:22" "01:19:56")
+    (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "01:25:20" "01:26:13")
+    (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "01:32:42" "01:36:26")
+    (->snip "/home/benj/video/2025-02-08_18-54-15.mkv" "01:44:56" nil))
+   (doit! "/tmp/trs-4-3.mp4"))
+
+  (->
+   (concat-snips
+    (->snip "/tmp/trs-4.mp4" nil nil)
+    (speed-snip
+     (->snip "/home/benj/video/2025-02-08_15-14-08.mkv" "00:00:08" nil)
+     1.5)
+    (->snip "/tmp/trs-4-3.mp4" nil nil))
+   (doit! "/tmp/trs-4-full.mp4"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  (doit!
+   (->
+    (concat-snips
+     (->snip "/home/benj/video/2025-02-14_14-01-27.mkv" "00:00:02" "00:03:00")
+     (->snip "/home/benj/video/2025-02-14_14-01-27.mkv" "00:03:40" "00:04:54")
+     (->snip "/home/benj/video/2025-02-14_14-01-27.mkv" "00:07:39" "00:09:45")
+     (->snip "/home/benj/video/2025-02-14_14-01-27.mkv" "00:11:09" nil)
+     (->snip "/home/benj/video/2025-02-15_17-10-10.mkv" "00:00:25" "00:01:16")
+     (->snip "/home/benj/video/2025-02-15_17-10-10.mkv" "00:01:31" nil)
+     (->snip "/home/benj/video/2025-02-14_14-14-46.mkv" nil "00:10:02")
+     (->snip "/home/benj/video/2025-02-14_14-14-46.mkv" "00:10:13" "00:15:55")
+     (->snip "/home/benj/video/2025-02-14_14-14-46.mkv" "00:17:12" "00:27:17")
+     (->snip "/home/benj/video/2025-02-14_14-14-46.mkv" "00:27:21" "00:29:11")
+
+     (->snip "/home/benj/video/2025-02-15_17-37-46.mkv" "00:00:02" "00:01:41")
+
+
+     (->snip "/home/benj/video/2025-02-15_13-19-18.mkv" "00:00:44" "00:04:05")
+     (->snip "/home/benj/video/2025-02-15_13-19-18.mkv" "00:05:44" "00:12:08")
+     (->snip "/home/benj/video/2025-02-15_13-19-18.mkv" "00:29:43" "00:33:20")
+     (->snip "/home/benj/video/2025-02-15_13-19-18.mkv" "00:38:27" "00:39:44")
+
+     (->snip "/home/benj/video/2025-02-15_14-16-14.mkv" "00:00:28" "00:07:20")
+     (->snip "/home/benj/video/2025-02-15_14-16-14.mkv" "00:08:50" "00:18:12")
+     (->snip "/home/benj/video/2025-02-15_14-16-14.mkv"
+             "00:19:38"
+             nil
+             ))
+    )
+   "/tmp/emacs-gemini-quick-4.mkv")
+
+  ;; #object[sun.nio.fs.UnixPath 0x1f6de36a "/tmp/emacs-gemini-quick-4.mkv"]
+
+  (doit!
+   (speed-snip
+    (->snip "/tmp/emacs-gemini-quick-4.mkv" nil nil)
+    1.85)
+   "/tmp/egq.mp4")
+
+
+  ;; #object[sun.nio.fs.UnixPath 0x41aebfed "/tmp/emacs-gemini-quick-3.mkv"]
+
+  (speed-snip
+   (->snip "/home/benj/video/2025-02-16_10-27-34.mkv" "00:00:15" "00:08:23")
+   1.8)
+  ;; need background?
+
+
+  (doit!
+   (speed-snip
+    (concat-snips
+     (->snip "/home/benj/video/2025-02-16_10-27-34.mkv" "00:08:57" "00:17:18")
+     (->snip "/home/benj/video/2025-02-16_10-27-34.mkv" "00:22:16" "00:28:56")
+     (->snip "/home/benj/video/2025-02-16_10-27-34.mkv" "00:41:03" nil)
+     (->snip "/home/benj/video/2025-02-16_11-12-48.mkv" nil nil))
+    1.8)
+   "g-stream-2.mkv")
+
+
+
+  (doit!
+   (concat-snips
+    (speed-snip (->snip "/home/benj/video/2025-02-16_10-27-34.mkv" "00:00:15" "00:08:23") 1.8)
+    (->snip
+     "/home/benj/repos/clipspeak/g-stream-mixed-out.mkv" nil nil)
+    )
+   "g-stream.mkv")
+
+
+  ;; #object[sun.nio.fs.UnixPath 0x71d9c24a "/home/benj/repos/clipspeak/g-stream.mkv"]
+
+  ;; /benj/tmp/mixed_audio-gstream-2llllllllllllli.mkv
+
+
+
+
+
+
+  )
 
 
 (comment
 
-
   (doit!
-   (concat-snips
-    (->snip "/home/benj/video/2024-12-02-12-10-37.mkv" nil "00:09:14")
-    (->snip "/home/benj/video/2024-12-02-12-22-01.mkv" nil "00:01:45")
-    (->snip "/home/benj/video/2024-12-02-12-25-35.mkv" nil "00:10:29")
-    (->snip "/home/benj/video/2024-12-02-12-36-29.mkv" nil "00:02:04")
-    (->snip "/home/benj/video/2024-12-02-12-39-38.mkv" nil "00:30:22")
-    (->snip "/home/benj/video/2024-12-02-18-07-37.mkv" nil "00:26:08")
-    (->snip "/home/benj/video/2024-12-02-18-34-08.mkv" nil "00:03:09")
-    (->snip "/home/benj/video/2024-12-02-18-38-20.mkv" "00:00:11" "00:42:49")
+   (speed-snip
+    (concat-snips
+     (->snip "/home/benj/video/2025-02-21_10-58-58.mkv" "00:01:23" "00:05:42")
+     (->snip "/home/benj/video/2025-02-21_11-07-14.mkv" "00:00:29" nil))
+    1.5
     )
-   "metta-part-3.mp4")
-
-
-
-  ;; next part
-
-  (doit!
-   (concat-snips
-    (->snip "/home/benj/video/2024-12-04-20-04-06.mkv" nil "00:04:07")
-    (->snip "/home/benj/video/2024-12-05-09-36-02.mkv" nil "00:04:49")
-
-    (->snip "/home/benj/video/2024-12-05-09-36-02.mkv" "00:14:33" "00:36:57")
-
-    (->snip "/home/benj/video/2024-12-05-09-36-02.mkv" "00:38:03" "00:46:38")
-
-
-    (->snip "/home/benj/video/2024-12-05-10-26-59.mkv" nil "00:02:21")
-    (->snip "/home/benj/video/2024-12-05-10-26-59.mkv" "00:07:16" "00:24:40")
-    (->snip "/home/benj/video/2024-12-05-10-26-59.mkv" "00:38:55" "00:46:06")
-
-    (->snip "/home/benj/video/2024-12-05-10-26-59.mkv" "00:49:40" "00:51:29")
-
-
-    (->snip "/home/benj/video/2024-12-05-10-26-59.mkv" "00:53:28" "00:59:44")
-
-    (->snip "/home/benj/video/2024-12-05-13-44-24.mkv" nil "00:47:57")
-    (->snip "/home/benj/video/2024-12-05-13-44-24.mkv" "00:51:03" "00:56:21")
-
-    (->snip "/home/benj/video/2024-12-05-13-44-24.mkv" "00:59:08" nil))
-   "aoc-day4.mp4")
-
-
-  (doit!
-   (concat-snips
-    (->snip "/home/benj/repos/clipspeak/day6part1.mp4" "00:53:25" "00:53:51")
-    (->snip "/home/benj/repos/clipspeak/day6part1.mp4" nil nil))
-   "aoc-day6p1.mp4"
-   )
-
-
-  ;; non deterministic results
-
-
-  (doit!
-   (concat-snips
-    (->snip "/home/benj/video/2024-12-07-12-14-11.mkv" "00:12:32" nil)
-    (->snip "/home/benj/video/2024-12-07-13-06-58.mkv" nil "00:25:31"))
-   "metta-nond.mp4")
-
-
-  ;; #object[sun.nio.fs.UnixPath 0x5705e310 "/home/benj/repos/clipspeak/metta-nond.mp4"]
-
-
-  ;; sound:
-  ;; https://soundcloud.com/mariuszpierog
-
-
-  (doit!
-   (concat-snips
-    (->snip "/home/benj/video/2024-12-08-10-18-16.mkv" nil "00:02:33")
-    (->snip "/home/benj/video/2024-12-08-10-18-16.mkv" "00:03:30" "00:07:03")
-    (->snip "/home/benj/video/2024-12-08-10-18-16.mkv" "00:10:18" "00:20:45")
-    (->snip "/home/benj/video/2024-12-08-10-18-16.mkv" "00:23:46" "00:31:28")
-    (->snip "/home/benj/video/2024-12-08-10-59-35.mkv" "00:01:05" "00:36:49")
-    (->snip "/home/benj/video/2024-12-08-10-59-35.mkv" "00:44:18" nil))
-   "metta-control-flow.mp4")
-
-
+   "revert-buffer2.mp4")
 
 
 
   (doit!
-   (->snip "/home/benj/video/2024-12-08-17-00-56.mkv" "00:00:05" nil)
+   (speed-snip
+    (concat-snips
 
-   "example.mp4")
-
-  ;; ffmpeg -i video.mp4 -i audio1.wav -i audio2.wav -i audio3.wav \
-  ;; -map 0:v -map 0:a -map 1:a -map 2:a -map 3:a \
-  ;; -c:v copy -c:a copy \
-  ;; output.mp4
-
-  ;; ffmpeg -i originalfile.mov -c:v copy -c:a aac -b:a 160k -ac 2 -filter_complex amerge=inputs=2 output.mp4
+     (->snip "/home/benj/video/2025-02-25_14-18-33.mkv" "00:00:05" "00:02:03")
 
 
-  ;;
-  ;; ffmpeg -i /home/benj/video/2024-12-08-17-47-15.mkv -c:v copy -c:a aac -b:a 160k -/home/benj/video/2024-12-08-18-27-46.mkv2 -filter_complex amerge=inputs=2 output2.mp4
-
-  (doit!
-   (concat-snips
-    (->snip "/home/benj/video/output2.mp4" nil "00:29:55")
-    (->snip "/home/benj/repos/clipspeak/metta-python-2.mkv" nil "00:30:51"))
-   "metta-python-p1.mp4")
-
-  ;; #object[sun.nio.fs.UnixPath 0x314ee71f "/home/benj/repos/clipspeak/metta-python-p1.mp4"]
-
-  (doit!
-   (concat-snips
-    (->snip "/home/benj/repos/clipspeak/parser2.mkv" nil "00:09:48")
-    (->snip "/home/benj/repos/clipspeak/parsing-and-interpretation.mkv" nil "00:32:40")
-    (->snip "/home/benj/repos/clipspeak/parsing-and-interpretation.mkv" "00:44:47" "01:17:18")
-    (->snip "/home/benj/repos/clipspeak/parsing-and-interpretation.mkv" "01:25:00" nil))
-   "metta-python-p2.mp4")
-
-
-
-  (->snip "/home/benj/notes/output.wav")
-
-
-  ;; #object[sun.nio.fs.UnixPath 0x35aba98f "/home/benj/repos/clipspeak/day-7-aoc.mp4"]
-
-
-
-
-
-
-
-
-
-
-
-
-  (doit!
-   (concat-snips
-    (->snip "/home/benj/video/2024-12-14-20-56-52.mkv"
-            "00:00:55"
-            "00:51:05")
-    (->snip "/tmp/0001-74303.mp4" nil nil))
-   "aoc-day14.mp4")
-
-
-  ;; #object[sun.nio.fs.UnixPath 0x6d2edd0 "/home/benj/repos/clipspeak/aoc-day14.mp4"]
-
-
-  ;; # Generate left channel tone (400 Hz)
-  ;; ffmpeg -f lavfi -i "sine=frequency=200:duration=300" -c:a pcm_s16le left_tone.wav
-
-  ;; # Generate right channel tone (440 Hz - creating a 40 Hz gamma beat)
-  ;; ffmpeg -f lavfi -i "sine=frequency=240:duration=300" -c:a pcm_s16le right_tone.wav
-
-  ;; # Combine the tones into stereo binaural beat
-
-  ;; ffmpeg -i left_tone.wav -i right_tone.wav -filter_complex "[0:a][1:a]join=inputs=2:channel_layout=stereo[a]" -map "[a]" binaural_beat.wav
-
-
-  ;; # Lower the volume of the binaural beat (optional but recommended)
-  ;; ffmpeg -i binaural_beat.wav -filter:a "volume=0.3" binaural_beat_quiet.wav
-
-  ;; # Mix the binaural beat with your existing audio file
-  ;; # Replace "your_audio.mp3" with your actual audio file
-
-  ;; ffmpeg -i your_audio.mp3 -i binaural_beat_quiet.wav -filter_complex \
-  ;; "[0:a][1:a]amix=inputs=2:duration=first:weights=1 0.5[a]" \
-  ;; -map "[a]" output_with_binaural.mp3
-
-  ;; # Clean up temporary files
-  ;; rm left_tone.wav right_tone.wav binaural_beat.wav binaural_beat_quiet.wav
-
-
-  ;; ffmpeg  -i input.mp4 -stream_loop -1 -i input.mp3 -shortest -map 0:v:0 -map 1:a:0 -y out.mp4
-
-
-
-  (->
-   (->snip "/home/benj/video/2025-01-11-10-08-54.mkv" nil nil)
-   (speed-snip 1.5)
-   (volume-snip 1.5)
-   (doit! "trs1.mp4"))
-
-  (->
-   (concat-snips
-    (->snip "/home/benj/video/2025-01-11-18-00-44.mkv" nil "00:02:50")
-    (->snip "/home/benj/video/2025-01-11-18-00-44.mkv" "00:08:34" nil))
-   (doit! "type-pi-pt2.mp4"))
-
-
-
-
-
-
-
-
-  (->
-
-   (speed-snip 2)
-   (doit! "learning-pi-1.mp4"))
-
-  (->
-
-   (concat-snips
-    (->snip "/home/benj/video/output.mp4" nil "00:03:25")
-
-    (->snip "/home/benj/video/output.mp4" "00:05:52" "00:05:55")
-    (->snip "/home/benj/video/output.mp4" "00:06:22" "00:14:57")
-    (->snip "/home/benj/video/output.mp4" "00:17:27" "00:23:20")
-
-    (->snip "/home/benj/video/output.mp4" "00:23:20" "00:23:35")
-    (->snip "/home/benj/video/output.mp4" "00:25:46" "00:29:31")
-    (->snip "/home/benj/video/output.mp4" "00:31:26" "00:40:49")
-    (->snip "/home/benj/video/output.mp4" "00:43:40" "00:44:10"))
-   (speed-snip 2)
-   (doit! "overtone-1.mp4"))
-
-  (->
-   (concat-snips
-    (->snip "/home/benj/video/output.mp4"
-            "00:44:10"
-            "00:59:29")
-    (->snip "/home/benj/video/output.mp4" "01:00:12" nil))
-   (doit! "overtone-2.mp4"))
-
-
-
-  (->
-   (concat-snips
-    (->snip "/home/benj/trs-output.mp4" nil "00:46:31")
+     (->snip "/home/benj/video/2025-02-25_14-18-33.mkv"
+             ""
+             )
+     )
+    1.5
     )
-   (doit! "trs-2.mp4"
-          ))
+   "revert-buffer2.mp4")
+
+  (->
+   (->snip "/home/benj/video/2025-03-07_09-58-39.mkv" nil nil)
+   (speed-snip 2.5)
+   (doit! "foo.mp4"))
+
+  ;; #object[sun.nio.fs.UnixPath 0x7f7cb5d8 "/home/benj/repos/clipspeak/foo.mp4"]
+
+  (->
+   (concat-snips
+    (->snip "/home/benj/repos/clipspeak/foo.mp4" nil "00:03:52")
+    (->snip "/home/benj/repos/clipspeak/foo.mp4" "00:04:18" nil))
+   (doit! "foo2.mp4"))
+  ;; #object[sun.nio.fs.UnixPath 0x7d65e59c "/home/benj/repos/clipspeak/foo2.mp4"]
+
+
+  ;; ----------------
+
+
+
+
+  (doit!
+   (concat-snips
+    (->snip "/home/benj/video/2025-04-05_12-45-21.mkv" nil "00:15:54")
+    (->snip "/home/benj/video/2025-04-05_12-45-21.mkv" "00:17:07" nil))
+   "/home/benj/video/wolfram-3.mp4")
+
+
+
+
+  (doit!
+   (concat-snips
+    (->snip "/home/benj/video/2025-05-10_09-51-48.mkv" nil "00:06:27")
+    (->snip "/home/benj/video/2025-05-10_09-51-48.mkv" "00:06:52" nil)
+    (->snip "/home/benj/video/2025-05-10_10-00-15.mkv" nil "00:02:56")
+    (->snip "/home/benj/video/2025-05-10_10-00-15.mkv" "00:03:57" nil)
+    (->snip "/home/benj/video/2025-05-10_10-06-51.mkv" nil "00:02:37")
+    (->snip "/home/benj/video/2025-05-10_10-06-51.mkv" "00:03:30" "00:08:52")
+    (->snip "/home/benj/video/2025-05-10_10-06-51.mkv" "00:10:12" "00:11:54")
+    (->snip "/home/benj/video/2025-05-10_10-06-51.mkv" "00:12:18" "00:34:52")
+    (->snip "/home/benj/video/2025-05-10_10-48-03.mkv" nil "00:06:23")
+    (->snip "/home/benj/video/2025-05-10_10-48-03.mkv" "00:08:15" "00:13:38")
+    (->snip "/home/benj/video/2025-05-10_10-48-03.mkv" "00:14:42" "00:14:59")
+    (->snip "/home/benj/video/2025-05-10_10-48-03.mkv" "00:04:00" "00:11:40")
+    (->snip "/home/benj/video/2025-05-10_10-48-03.mkv" "00:12:17" "00:14:16")
+
+    (->snip "/home/benj/video/2025-05-10_10-48-03.mkv" "00:15:02" "00:19:42")
+    (->snip "/home/benj/video/2025-05-10_10-48-03.mkv" "00:20:18" nil)
+    (->snip "/home/benj/video/2025-05-10_11-05-29.mkv" "00:00:06" nil))
+   "/tmp/foo.mp4")
+
+
+  (doit!
+   (concat-snips
+
+    (->snip "/home/benj/video/2025-06-20_17-27-12.mkv" "01:01:40" "01:02:56")
+
+    (->snip "/home/benj/video/2025-06-20_17-27-12.mkv" "00:00:04" "00:00:19")
+    (->snip "/home/benj/video/2025-06-20_17-27-12.mkv" "00:02:45" "00:03:00")
+    (->snip "/home/benj/video/2025-06-20_17-27-12.mkv" "00:07:28" "00:07:50")
+    (->snip "/home/benj/video/2025-06-20_17-27-12.mkv" "00:08:38" "00:10:00")
+
+    (->snip "/home/benj/video/2025-06-20_17-27-12.mkv" "00:11:35" "00:12:20")
+
+
+
+
+
+
+
+    (->snip "/home/benj/video/2025-06-20_17-27-12.mkv" "00:12:39" "00:13:43")
+
+    (->snip "/home/benj/video/2025-06-20_17-27-12.mkv" "00:21:03" "00:23:20")
+
+
+    (->snip "/home/benj/video/2025-06-20_17-27-12.mkv" "00:30:40" "00:33:12")
+
+
+    (->snip "/home/benj/video/2025-06-20_17-27-12.mkv" "00:35:20" "00:41:05")
+
+
+    (->snip "/home/benj/video/2025-06-20_17-27-12.mkv" "00:44:50" "00:47:56")
+
+
+    (->snip "/home/benj/video/2025-06-20_17-27-12.mkv" "00:48:50" "00:49:20")
+
+
+    (->snip "/home/benj/video/2025-06-20_17-27-12.mkv" "00:52:01" "01:11:31")
+
+
+    (->snip "/home/benj/video/2025-06-20_17-27-12.mkv" "01:15:30" "01:15:40")
+
+    (->snip "/home/benj/video/2025-06-20_17-27-12.mkv" "01:17:05" "01:20:48")
+
+
+    ;; (->snip "/home/benj/video/2025-06-20_17-27-12.mkv" "00:19:25" "00:19:57")
+    ;; (->snip "/home/benj/video/2025-06-20_17-27-12.mkv" "00:19:25" "00:19:57")
+
+
+    )
+   "/tmp/foo.mp4")
+
+#object[sun.nio.fs.UnixPath 0x17de5938 "/tmp/foo.mp4"]
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
